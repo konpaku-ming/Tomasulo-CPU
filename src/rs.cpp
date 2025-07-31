4be1c416e368d41c6138eb96504f48ce7967bfed
Youming <dxuanming@sjtu.edu.cn>
2025-07-31 10:34:35 +0800

    add independent unit ALU

--- src/rs.cpp
+++ src/rs.cpp
@@ -53,37 +53,5 @@ namespace cpu_sim {
     }
   }
 
-  /*
-  void ReservationStation::run() {
-    if (now_rs.empty())return;
-    for (int i = 0; i < kRSSize; i++) {
-      if (!now_rs.exist(i))continue;
-      //检查依赖
-      const auto cur = now_rs[i];
-      if (cur.qj != -1 || cur.qk != -1)continue;
-      if (cur.inst.op == kHalt) {
-        const auto dest = cur.rob_dest;
-        rob.next_rob[dest].progress = 3; //一周期
-        rob.next_rob[dest].status = kWrite;
-        //无计算
-        next_rs.remove(i);
-        return;
-      }
-      //ALU运算
-      const auto res = alu_calc(cur.inst.op, cur.vj, cur.vk, cur.a);
-      const auto dest = cur.rob_dest;
-      rob.next_rob[dest].progress = 3; //一周期
-      if (cur.inst.op == kJal || cur.inst.op == kJalr) {
-        rob.next_rob[dest].pos = res; //新pc作为pos值返回
-        rob.next_rob[dest].value = cur.inst.pc + 4;
-      } else {
-        rob.next_rob[dest].value = res;
-      }
-      rob.next_rob[dest].status = kWrite;
-      next_rs.remove(i);
-    }
-  }
-  */
-
   ReservationStation rs;
 }
